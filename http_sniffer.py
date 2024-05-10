99e2ce500d7583221490e8f2b89ad97bfc00eeb0
mofayad96 <117763149+mofayad96@users.noreply.github.com>
2024-05-10 12:19:26 +0300

    Update http_sniffer.py

--- http_sniffer.py
+++ http_sniffer.py
@@ -1,24 +1,30 @@
 import scapy.all as scapy
+
 from scapy.layers import http
 
-def packet_callback(packet):
-    if packet.haslayer(IP):
-        ip_src = packet[IP].src
-        ip_dst = packet[IP].dst
-        print(f"Source IP: {ip_src}, Destination IP: {ip_dst}")
 
-def sniffer(interface):
-    print("-----------------------------------")
+
+
+def sniffer(interface): 
+    print("-------------mo-------------------")
     print("-------Sniffer Has Started---------")
-    print("-----------------------------------")
+    print("-----------------salah---------------")
+    
     scapy.sniff(iface=interface, store=False, prn=process)
+    
 
+
+#Sniffing http packets
 def process(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print("[+] ", packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path)
-        if packet.haslayer(scapy.Raw):
-            request = packet[scapy.Raw].load 
-            print("[*_*] ->->->->-> ", request)
 
-# Sniffing packets
+    if packet.haslayer(http.HTTPRequest):
+ 
+        print("[+] ",packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path) 
+       
+        if packet.haslayer(scapy.Raw):
+       
+            request  = packet[scapy.Raw].load 
+            
+            print("[*_*] ->->->->-> ",request)
+
 sniffer("Wi-Fi")
